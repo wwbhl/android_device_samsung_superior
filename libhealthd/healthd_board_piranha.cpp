28e062d9de1478577a7d165e2e4b7ed9e03b21e9
baohailong <baohailong@sina.com>
2015-12-16 16:14:50 +0800

    add healthd def
    Change-Id: If8d50d04609882c86865db9be492b9bc7afa6e11

--- libhealthd/healthd_board_piranha.cpp
+++ libhealthd/healthd_board_piranha.cpp
@@ -29,9 +29,16 @@ void healthd_board_init(struct healthd_config *config)
     config->batteryTechnologyPath = "/sys/class/power_supply/battery/technology";
     // others use defaults
 }
+void healthd_board_mode_charger_set_backlight(bool en);
+
+void healthd_board_mode_charger_battery_update(struct android::BatteryProperties *batt_prop);
+
+void healthd_board_mode_charger_draw_battery(struct android::BatteryProperties *batt_prop);
+
+void healthd_board_mode_charger_init(void);
 
 
-int healthd_board_battery_update(struct android::BatteryProperties *props)
+healthd_board_battery_update(struct android::BatteryProperties *props)
 {
     // return 0 to log periodic polled battery status to kernel log
     return 0;
